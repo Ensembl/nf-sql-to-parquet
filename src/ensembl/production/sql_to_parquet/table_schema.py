54e7c31feaa9eb349f5117a71ea20d08cbb73b45
Sukanya Denni <sukanya@ebi.ac.uk>
2024-07-24 10:17:40 +0100

    update compara schema

--- src/ensembl/production/sql_to_parquet/table_schema.py
+++ src/ensembl/production/sql_to_parquet/table_schema.py
@@ -16,7 +16,7 @@ class GeneSchema:
             ('nomenclature_symbol', pa.string()),
             ('nomenclature_provider', pa.string()),
             ('canonical_transcript', pa.string()),
-
+            # Supplementary data columns
             ('transcript_stable_ids', pa.list_(pa.string())),
             ('translation_stable_ids', pa.list_(pa.string())),
             ('synonym', pa.list_(pa.string())),
@@ -25,7 +25,7 @@ class GeneSchema:
             ('proj_parent_gene', pa.list_(pa.string())),
             ('alternative_name', pa.list_(pa.string())),
             ('go_terms', pa.list_(pa.string())),
-
+            # Partition column
             ('species', pa.string()),
         ])
 
@@ -42,7 +42,7 @@ class TranscriptSchema:
             ('source', pa.string()),
             ('description', pa.string()),
             ('transcript_symbol', pa.string()),
-
+            # Supplementary data columns
             ('translation_stable_ids', pa.list_(pa.string())),
             ('miRNA_coordinates', pa.list_(pa.string())),
             ('frameshift', pa.list_(pa.string())),
@@ -50,7 +50,7 @@ class TranscriptSchema:
             ('MANE_select', pa.list_(pa.string())),
             ('MANE_plus_clinical', pa.list_(pa.string())),
             ('go_terms', pa.list_(pa.string())),
-
+            # Partition column
             ('species', pa.string()),
         ])
 
@@ -61,9 +61,9 @@ class TranslationSchema:
             ('stable_id', pa.string()),
             ('start', pa.int64()),
             ('end', pa.int64()),
-           
+            # Supplementary data columns
             ('go_terms', pa.list_(pa.string())),
-
+            # Partition column
             ('species', pa.string()),
         ])
 
@@ -73,18 +73,17 @@ class ComparaSchema:
             ('homology_id', pa.int64()),
             ('description', pa.string()),
             ('type', pa.string()),
-            ('dn', pa.int64()),
-            ('ds', pa.int64()),
+            ('dn', pa.float64()),
+            ('ds', pa.float64()),
             ('goc_score', pa.int8()),
-            ('wga_coverage', pa.int64()),
-            ('is_high_confidence', pa.int8),
+            ('wga_coverage', pa.float64()),
+            ('is_high_confidence', pa.int8()),
             ('stable_id', pa.string()),
-            ('perc_cov', pa.int64()),
-            ('perc_id', pa.int64()),
+            ('perc_cov', pa.float64()),
+            ('perc_id', pa.float64()),
             ('homologue_stable_id', pa.string()),
-            ('homologue_perc_cov', pa.int64()),
-            ('homologue_perc_id', pa.int64()),
-
-            ('species', pa.string()),
+            ('homologue_perc_cov', pa.float64()),
+            ('homologue_perc_id', pa.float64()),
+            # Partition column
+            ('species', pa.string())
         ])
-
